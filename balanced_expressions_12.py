b410a3a979ba59784d012affab5f50ea5c646a4a
Jorge Alejandro Uribe Solórzano <jorge.uribework@hotmail.com>
2024-01-23 11:44:26 -0600

    Starting to work on balanced_expressions_12.py

---
+++ balanced_expressions_12.py
@@ -0,0 +1,20 @@
+"""-------------------------------------------------------------------
+Crea una función que reciba dos cadenas como parámetro (str1, str2)
+ * e imprima otras dos cadenas como salida (out1, out2).
+ * - out1 contendrá todos los caracteres presentes en la str1 pero NO
+ *   estén presentes en str2.
+ * - out2 contendrá todos los caracteres presentes en la str2 pero NO
+ *   estén presentes en str1.
+----------------------------------------------------------------------"""
+
+def balanced_expressions(str1, str2):
+    print(str1)
+    print(str2)
+
+
+
+str1 = input("Escribe una palabra, frase u oración que te guste >>> ")
+str2 = input("Escribe otra palabra, frase u oración que te guste >>> ")
+
+balanced_expressions(str1, str2)
+
